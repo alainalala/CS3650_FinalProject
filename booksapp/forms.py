89c219e37ff65d75c7d24822a1efefe9d604aa35
Alaina Larsen <alainalarsen@mail.weber.edu>
2022-12-07 22:19:10 -0700

    accessibility fixes

--- booksapp/forms.py
+++ booksapp/forms.py
@@ -9,14 +9,17 @@ class StartBookForm (forms.ModelForm):
         fields = ['read_status', 'owned', 'date_started']
         widgets = {
             'read_status': forms.Select(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputReadStatus'
             }),
             'owned': forms.CheckboxInput(attrs={
-                'class': "form-control form-check-input"
+                'class': "form-control form-check-input",
+                'id': 'inputOwned'
             }),
             'date_started': forms.NumberInput(attrs={
                 'class': "form-control",
-                'type': 'date'
+                'type': 'date',
+                'id': 'inputDateStarted'
             }),
         }
 
@@ -28,27 +31,34 @@ class FinishBookForm(forms.ModelForm):
         widgets = {
             'owned': forms.CheckboxInput(attrs={
                 'class': "form-check-input form-control",
+                'id': 'inputOwned'
             }),
             'read_status': forms.Select(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputReadStatus'
             }),
             'rating': forms.NumberInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputRating'
             }),
             'notes': forms.Textarea(attrs={
                 'class': "form-control",
-                'rows': 3
+                'rows': 3,
+                'id': 'inputNotes'
             }),
             'date_started': forms.NumberInput(attrs={
                 'class': "form-control",
-                'type': 'date'
+                'type': 'date',
+                'id': 'inputDateStarted'
             }),
             'date_finished': forms.NumberInput(attrs={
                 'class': "form-control",
-                'type': 'date'
+                'type': 'date',
+                'id': 'inputDateFinished'
             }),
             'times_read': forms.NumberInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputTimesRead'
             }),
         }
 
@@ -61,62 +71,80 @@ class BookForm(forms.ModelForm):
                   'date_started', 'date_finished', 'times_read']
         widgets = {
             'user': forms.Select(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputUser'
             }),
             'title': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputTitle'
             }),
             'author': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputAuthor'
             }),
             'genre': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputGenre'
             }),
             'description': forms.Textarea(attrs={
                 'class': "form-control",
-                'rows': 3
+                'rows': 3,
+                'id': 'inputDescription'
             }),
             'image': forms.FileInput(attrs={
-                'class': "form-control-file"
+                'class': "form-control-file",
+                'id': 'inputImage'
             }),
             'page_count': forms.NumberInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputPageCount'
             }),
             'recommended_by': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputRecommended'
             }),
             'series_name': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputSeriesName'
             }),
             'series_number': forms.NumberInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputSeriesNumber'
             }),
             'owned': forms.CheckboxInput(attrs={
-                'class': "form-check-input"
+                'class': "form-check-input",
+                'id': 'inputOwned'
             }),
             'date_added': forms.NumberInput(attrs={
                 'class': "form-control",
-                'type': 'date'
+                'type': 'date',
+                'id': 'inputDateAdded'
             }),
             'read_status': forms.Select(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputReadStatus'
             }),
             'rating': forms.NumberInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputRating'
             }),
             'notes': forms.Textarea(attrs={
                 'class': "form-control",
-                'rows': 3
+                'rows': 3,
+                'id': 'inputNotes'
             }),
             'date_started': forms.NumberInput(attrs={
                 'class': "form-control",
-                'type': 'date'
+                'type': 'date',
+                'id': 'inputDateStarted'
             }),
             'date_finished': forms.NumberInput(attrs={
                 'class': "form-control",
-                'type': 'date'
+                'type': 'date',
+                'id': 'inputDateFinished'
             }),
             'times_read': forms.NumberInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputTimesRead'
             }),
         }
--- users/forms.py
+++ users/forms.py
@@ -6,33 +6,39 @@ from django.apps import apps
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'inputEmail'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'inputPassword1'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'inputPassword2'}))
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
         widgets = {
             'username': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputUsername'
             }),
             'first_name': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputFirstName'
             }),
             'last_name': forms.TextInput(attrs={
-                'class': "form-control"
+                'class': "form-control",
+                'id': 'inputLastName'
             }),
             'email': forms.EmailInput(attrs={
                 'class': "form-control",
-                'type': "email"
+                'type': "email",
+                'id': 'inputEmail'
             }),
             'password1': forms.PasswordInput(attrs={
                 'class': "form-control",
-                'type': 'password'
+                'type': 'password',
+                'id': 'inputPassword1'
             }),
             'password2': forms.PasswordInput(attrs={
                 'class': "form-control",
-                'type': 'password'
+                'type': 'password',
+                'id': 'inputPassword2'
             }),
         }
